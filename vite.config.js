037c5cc1134db327026139abc01246cb16d80f8b
Samriddha Ganguly <117953263+QuantumPopsci@users.noreply.github.com>
2025-11-04 03:57:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import sitemap from 'vite-plugin-sitemap'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-     require('@tailwindcss/line-clamp'),
-  require('@tailwindcss/forms'),
-  require('@tailwindcss/typography'),
     react(),
     sitemap({ hostname: 'https://samriddhas-quantum-realm.vercel.app' })
   ],
