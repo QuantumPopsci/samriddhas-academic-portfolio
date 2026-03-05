5762ed4d79fa1b9e1895a293ee71982abf5ec2b8
Samriddha Ganguly <117953263+QuantumPopsci@users.noreply.github.com>
2026-03-05 18:46:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useMemo, useEffect, useRef } from 'react';
 import Plot from 'react-plotly.js';
-import MeissnerBackground from "./MeissnerBackground";
+import SuperconductorField from "./components/SuperconductorField";
 import { Mail, Phone, Github, Linkedin, Sun, Moon, Menu, X, Code, BrainCircuit, Atom, Waves, ArrowLeft, ChevronLeft, ChevronRight, Download } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -1580,7 +1580,7 @@ const navLinks = [ { id: 'home', title: 'Home' }, { id: 'research', title: 'Rese
   return (
     <div className="relative min-h-screen text-slate-800 dark:text-slate-200 font-sans transition-colors duration-300">
       {/* ===== BACKGROUND BLOBS ===== */}
-      <MeissnerBackground />
+    <SuperconductorField />
       <div className="relative z-10">
         <header className="bg-white/60 dark:bg-slate-900/60 backdrop-blur-md dark:bg-slate-900/80 backdrop-blur-sm sticky top-0 z-50 shadow-md">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8">
